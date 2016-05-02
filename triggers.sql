374c06147920b5c596eb0417533118bc1a035a98
jason yang <jasonyang5394@gmail.com>
2016-05-02 17:40:18 -0400

    readded trigger topbid

--- triggers.sql
+++ triggers.sql
@@ -3,6 +3,7 @@ DROP TRIGGER IF EXISTS hiddenmin;
 DROP TRIGGER IF EXISTS topbid;
 DROP TRIGGER IF EXISTS autobid;
 
+
 DELIMITER |
 CREATE TRIGGER hiddenmin AFTER INSERT ON Bid
 FOR EACH ROW 
@@ -22,6 +23,23 @@ END;
 |
 DELIMITER ;
 
+DELIMITER | 
+CREATE TRIGGER topbid AFTER INSERT ON Bid 
+FOR EACH ROW
+BEGIN
+	DECLARE top_bid FLOAT(8,2) DEFAULT 0;
+    SET top_bid = 
+    (SELECT top_bid FROM Auction WHERE auction_id=NEW.auction_id);
+    IF (NEW.bid_amount > top_bid OR top_bid IS NULL) THEN
+		UPDATE Auction SET Auction.top_bid = NEW.bid_amount,
+        Auction.bidder=NEW.bidder
+        WHERE Auction.auction_id = NEW.auction_id;
+    END IF;
+END;
+|
+DELIMITER ;
+
+
 DELIMITER |
 CREATE TRIGGER autobid AFTER INSERT ON UpperLimit
 FOR EACH ROW
@@ -39,7 +57,7 @@ BEGIN
     );
     IF (max_others > min_bid) THEN
 		SET max_final = max_others;
-	ELSE
+	ELSEIF (min_bid >= max_others) THEN
 		SET max_final = min_bid;
 	END IF;
     IF (NEW.upper_limit >= max_final) THEN
