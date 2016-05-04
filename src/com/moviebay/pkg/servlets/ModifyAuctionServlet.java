182b81b00d0388b38d42ad4751ef668cac5079f5
jason yang <jasonyang5394@gmail.com>
2016-05-04 01:39:58 -0400

    no message

--- src/com/moviebay/pkg/servlets/ModifyAuctionServlet.java
+++ src/com/moviebay/pkg/servlets/ModifyAuctionServlet.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.moviebay.pkg.ApplicationDAO;
+import com.moviebay.pkg.Auction;
+import com.moviebay.pkg.Bid;
 
 /**
  * Servlet implementation class ModifyAuctionServlet
@@ -43,10 +45,12 @@ public class ModifyAuctionServlet extends HttpServlet {
 		String update_auction = "";
 	
 		boolean exit = false;
+		float hiddenmin;
 		
 		if (!duration_s.isEmpty()){
 			try{
-				Integer.parseInt(duration_s);
+				if (Integer.parseInt(duration_s) < 0)
+					exit = true;
 			} catch (NumberFormatException e){
 				request.setAttribute("badDuration", "Movie duration must be in minutes.");
 				exit = true;
@@ -56,7 +60,8 @@ public class ModifyAuctionServlet extends HttpServlet {
 		
 		if(!minprice_s.isEmpty()){
 			try{
-				Float.parseFloat(minprice_s);
+				if (Float.parseFloat(minprice_s) < 0);
+					exit = true;
 			} catch (NumberFormatException e){
 				request.setAttribute("badMinimumIncr", "Minimum increment must have a valid price amount.");
 				exit = true;
@@ -66,7 +71,8 @@ public class ModifyAuctionServlet extends HttpServlet {
 		
 		if (!hiddenmin_s.isEmpty()){
 			try{
-				Float.parseFloat(hiddenmin_s);
+				if ((hiddenmin=Float.parseFloat(hiddenmin_s)) < 0)
+					exit = true;
 			} catch (NumberFormatException e){
 				request.setAttribute("badHiddenMin", "Hidden minimum must have a valid price amount.");
 				exit = true;
@@ -99,7 +105,7 @@ public class ModifyAuctionServlet extends HttpServlet {
 			update_item += strings.get(strings.size()-1);
 			update_item += "WHERE auction_id=" + auctionId + " AND item_id=" + itemId + ";";
 		}
-		
+		boolean hiddenminchanged = false;
 		//if user modified just one of the columns in Auction, create the string
 		if (auctionLength != null || !minprice_s.isEmpty() || !hiddenmin_s.isEmpty()){
 			LinkedList<String> strings = new LinkedList<String>(); 
@@ -108,8 +114,10 @@ public class ModifyAuctionServlet extends HttpServlet {
 				strings.add(" end_datetime= DATE_ADD(end_datetime, INTERVAL " + auctionLength + " DAY) ");
 			if (!minprice_s.isEmpty())
 				strings.add(" minimum_increment_price='" + minprice_s + "' ");
-			if (!hiddenmin_s.isEmpty())
+			if (!hiddenmin_s.isEmpty()){
+				hiddenminchanged = true;
 				strings.add(" hidden_minimum_price='" + hiddenmin_s + "' ");
+			}
 			for (int i = 0; i < strings.size()-1; ++i) {
 				update_auction += strings.get(i) + ",";
 			}
@@ -126,6 +134,23 @@ public class ModifyAuctionServlet extends HttpServlet {
 			if (!update_auction.isEmpty()){
 				dao.del_or_upd(update_auction);
 				request.setAttribute("updatedAuction", "Auction information updated.");
+				if (hiddenminchanged){
+					String hiddenminquery = "SELECT * FROM Auction WHERE auction_id=" + auctionId + ";";
+					Auction auction = dao.queryDB(hiddenminquery, Auction.class).get(0);
+					hiddenmin = Float.parseFloat(hiddenmin_s);
+					if (hiddenmin <= auction.getTopBid()){
+						String find_bidder = "SELECT * FROM Bid WHERE auction_id=" + auctionId + " AND "
+								+ "bid_amount=(SELECT MAX(bid_amount) FROM Bid WHERE auction_id=" + auctionId + ");";
+						Bid bid = dao.queryDB(find_bidder, Bid.class).get(0);
+						String change_auction = "UPDATE Auction SET top_bid=" + bid.getBidAmount() + ", " +
+								"winner='" + bid.getBidder() + "';";
+						dao.del_or_upd(change_auction);
+					}
+					else {
+						String change_auction = "UPDATE Auction SET winner=NULL";
+						dao.del_or_upd(change_auction);
+					}
+				}
 			}
 		} catch (SQLException e){
 			e.printStackTrace();
